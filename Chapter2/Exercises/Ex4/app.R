624a1e969487b49ed70e14680936fafd54120451
chsafouane <chsafouane@gmail.com>
2019-09-01 02:22:24 +0200

    Added chap1_ex4

---
+++ Chapter2/Exercises/Ex4/app.R
@@ -0,0 +1,38 @@
+library(shiny)
+
+
+# The takes two inputs values from the user, x and y
+# and diplays in a textOutputs the:
+#   - product of x and y
+#   - product of x and y plus 5
+#   - product of x and y plus 10
+# One could calculate the product only (instead of 3 times) 
+# once inside a reactive expression (so that that the 
+# 'variable' reacts to changes in x and/or y)
+
+ui <- fluidPage(
+  sliderInput("x", "If x is", min = 1, max = 50, value = 30),
+  sliderInput("y", "and y is", min = 1, max = 50, value = 5),
+  "then, (x * y) is", textOutput("product",inline = T),br(),
+  "and, (x * y) + 5 is", textOutput("product_plus5", inline = T), br(),
+  "and (x * y) + 10 is", textOutput("product_plus10", inline = T)
+)
+
+server <- function(input, output, session) {
+  
+  product <- reactive({
+    input$x * input$y
+  })
+  
+  output$product <- renderText({ 
+    product()
+  })
+  output$product_plus5 <- renderText({ 
+    product() + 5
+  })
+  output$product_plus10 <- renderText({ 
+    product() + 10
+  })
+}
+
+shinyApp(ui, server)
